01dbf5a455efe344e374d10bea24e02f03772d26
Hanif Al Irsyad <90784532+Hanifanta@users.noreply.github.com>
2022-10-16 11:37:58 +0700

    Update credentials.js

--- src/script/credentials.js
+++ src/script/credentials.js
@@ -1,4 +1,4 @@
-const client_id = "7f7aae6434034b259110873a4b4438a8";
-const client_secret = "fde4fa48ca62433f8b893bfdd63c104e";
+const client_id = "YOUR_CLIENT_ID";
+const client_secret = "YOUR_SECRET";
 
 export default { client_id, client_secret };
